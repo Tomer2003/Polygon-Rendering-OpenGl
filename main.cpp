56f0be78b3cee2780ee4af2590ee11e3a4fa25f1
Tomer <tomer12901@gmail.com>
2020-10-18 00:17:03 +0300

    added vertex array class files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-
-
 #include "Resources/GLEW/include/GL/glew.h"
 #include "Resources/GLFW/include/GLFW/glfw3.h"
 
@@ -12,6 +10,10 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
